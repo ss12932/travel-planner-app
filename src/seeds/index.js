0d8b553ebebbbee59f924cfe8aec8f04d74f93d4
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-07-27 21:38:07 +0100

    finished seeds script

--- src/seeds/index.js
+++ src/seeds/index.js
@@ -1,40 +1,45 @@
-const sequelize = require('../config/connection');
+const connection = require('../config/connection');
 const { Traveller, Location, Trip } = require('../models');
 
-const travellerSeedData = require('./travellerSeedData.json');
-const locationSeedData = require('./locationSeedData.json');
+const travellersData = require('./travellers.json');
+const locationsData = require('./locations.json');
 
 const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
+  //everytime you seed, drop tables + recreate table, clearing out any data.
+  await connection.sync({ force: true });
 
-  const travellers = await Traveller.bulkCreate(travellerSeedData);
+  console.log('DB sync successful');
 
-  const locations = await Location.bulkCreate(locationSeedData);
+  const travellers = await Traveller.bulkCreate(travellersData);
 
+  console.log('Travellers seed successful');
+  const locations = await Location.bulkCreate(locationsData);
+
+  console.log('locations seed successful');
   // Create trips at random
   for (let i = 0; i < 10; i++) {
     // Get a random traveller's `id`
-    const { id: randomTravellerId } = travellers[
-      Math.floor(Math.random() * travellers.length)
-    ];
+    const { id: randomTravellerId } =
+      travellers[Math.floor(Math.random() * travellers.length)];
 
     // Get a random location's `id`
-    const { id: randomLocationId } = locations[
-      Math.floor(Math.random() * locations.length)
-    ];
+    const { id: randomLocationId } =
+      locations[Math.floor(Math.random() * locations.length)];
 
-    // Create a new trip with random `trip_budget` and `traveller_amount` values, but with ids selected above
-    await Trip.create({
+    const newTrip = {
       trip_budget: (Math.random() * 10000 + 1000).toFixed(2),
       traveller_amount: Math.floor(Math.random() * 10) + 1,
       traveller_id: randomTravellerId,
       location_id: randomLocationId,
-    }).catch((err) => {
+    };
+    // Create a new trip with random `trip_budget` and `traveller_amount` values, but with ids selected above
+    await Trip.create(newTrip).catch((err) => {
       // If there's an error, such as the same random pairing of `traveller.id` and `location.id` occurring and we get a constraint error, don't quit the Node process
       console.log(err);
     });
   }
 
+  console.log('Trip seed sucessful');
   process.exit(0);
 };
 
