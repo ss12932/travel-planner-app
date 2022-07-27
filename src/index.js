5d2ccacbc3fa443ca78f176094704af89c94be78
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-07-27 16:55:17 +0100

    added routes + gitignore

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes = require('routes');
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT} 🚀 🌕`);
+});
