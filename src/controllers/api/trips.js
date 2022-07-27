d8c7f4ac09e1083c25bfac9c6fb9ed53188ff566
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-07-27 17:36:16 +0100

    trip routes linked to ctr

---
+++ src/controllers/api/trips.js
@@ -0,0 +1,11 @@
+const createTrip = (req, res) => {
+  res.send('createTrip');
+};
+const deleteTripById = (req, res) => {
+  res.send('deleteTripById');
+};
+
+module.exports = {
+  createTrip,
+  deleteTripById,
+};
