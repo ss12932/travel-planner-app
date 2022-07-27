623539eb7231bca5bf21155d13a2bd4ab56a025c
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-07-27 18:46:27 +0100

    connection to mysql done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const connection = require('./config/connection.js');
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 4000;
@@ -9,6 +10,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT} 🚀 🌕`);
-});
+const init = async () => {
+  try {
+    await connection.sync({ force: false });
+    // every connection dont drop the tables, just refresh connection. async op
+    console.log('[INFO]: DB connection successful');
+
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT} 🚀 🌕`);
+    });
+  } catch (err) {
+    console.log(`[ERROR]: DB connection failed | ${err.message}`);
+  }
+};
+
+init();
