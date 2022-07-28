92fe8d75a9174b0fc19f5c7ba58cde6a16d71130
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-07-28 11:28:44 +0100

    locations + trips done

--- src/controllers/api/locations.js
+++ src/controllers/api/locations.js
@@ -1,14 +1,77 @@
-const getLocations = (req, res) => {
-  res.send('getLocations');
+const { getPayloadWithValidFieldsOnly } = require('../../helpers');
+const { Traveller, Trip, Location } = require('../../models');
+
+const getLocations = async (req, res) => {
+  try {
+    const locations = await Location.findAll({});
+
+    return res.json({ data: locations });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get all locations | ${error.message}`);
+
+    return res.status(500).json({ error: 'Failed to get all locations' });
+  }
 };
-const createLocation = (req, res) => {
-  res.send('createLocation');
+
+const createLocation = async (req, res) => {
+  try {
+    const payload = getPayloadWithValidFieldsOnly(['location_name'], req.body);
+
+    if (Object.keys(payload).length !== 1) {
+      return res.status(400).json({ error: 'Bad request' });
+    }
+
+    await Location.create(payload);
+
+    return res.json({ data: 'Successfully created location' });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to create location | ${error.message}`);
+
+    return res.status(500).json({ error: 'Failed to create location' });
+  }
 };
-const getLocationById = (req, res) => {
-  res.send('getLocationById');
+
+const getLocationById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const location = await Location.findByPk(id, {
+      include: [
+        {
+          model: Traveller,
+          through: Trip,
+        },
+      ],
+    });
+
+    if (!location) {
+      return res.status(404).json({ error: 'Location not found' });
+    }
+
+    return res.json({ data: location });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get location | ${error.message}`);
+
+    return res.status(500).json({ error: 'Failed to get location' });
+  }
 };
-const deleteLocationById = (req, res) => {
-  res.send('deleteLocationById');
+
+const deleteLocationById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await Location.destroy({
+      where: {
+        id,
+      },
+    });
+
+    return res.json({ data: 'Successfully deleted location' });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to delete location | ${error.message}`);
+
+    return res.status(500).json({ error: 'Failed to delete location' });
+  }
 };
 
 module.exports = {
